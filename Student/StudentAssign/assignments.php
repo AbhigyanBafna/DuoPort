ecbeccb7b13e236f756d0f84a5136f68a00ddf77
Krish Manghani <krishmanghani@gmail.com>
2022-09-24 09:12:45 +0530

    changed ui

--- Student/StudentAssign/assignments.php
+++ Student/StudentAssign/assignments.php
@@ -1,4 +1,24 @@
-<!DOCTYPE html>
+<html>
+    <head> 
+    </head>
+    <style>
+        th{
+            border: 2px solid;
+            padding: 10px;
+            background: #04d3dd;
+        }
+        table{
+            border: 2px solid;
+            margin-left: 80px;
+        }
+        td{
+            border: 2px solid;
+            text-align: center;
+            background: #eeeeee;
+        }
+    </style>
+    <body>
+    <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
@@ -34,22 +54,47 @@
   </div>
 
     <h1 class="assgns-title">Assignments</h1>
-    <div class="assgns">
-      <div class="info">
-        <h3 class="assn_name">Assignment_name</h3>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non
-        </p>
-      </div>
-      <span class="deadline">
-        <h3>Deadline</h3>
-        <p id="demo"></p>
-      </span>
-    </div>
   </body>
 </html>
+
+        <table border="2">
+        <tr>
+        <th>Assignment Name</th>
+        <th>Subject</th>
+        <th>Description</th>
+        <th>Submission Date</th>
+        </tr>
+        
+
+
+<?php
+
+    $server = "localhost";
+    $username = "root";
+    $password = "";
+
+    $con = mysqli_connect($server, $username, $password);
+
+    $sql = "SELECT * FROM `csh`.`assignments`";
+    $data = mysqli_query($con, $sql);
+    $total = mysqli_num_rows($data);
+
+
+    
+
+    if($total > 0){
+        while($row = mysqli_fetch_assoc($data)){
+            echo "<tr><td>" . $row['name'] . "</td><td>" . $row['subject'] . "</td><td>" . $row['description'] . "</td><td>" . $row['date'] . "</td></tr>";
+            echo "<br>";
+        }
+    }
+
+    $con->close();
+
+?>
+</table>
+    </body>
+    
+</html>
+
+
